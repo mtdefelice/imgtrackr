423bf5fc3af46ff138bdbd5193c83b352f0306d0
Michael T. DeFelice <michael.defelice@gmail.com>
2017-09-13 18:47:38 -0500

    Initial commit ...

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, abort, send_from_directory
+import requests
+
+app = Flask (__name__)
+
+# Google Analytics Property Id ...
+_TID = 'UA-XXXXXX-X'
+
+@app.before_request
+def collect ():
+	d = {
+		'v': '1',
+		't': 'pageview',
+		'dp': request.headers.environ.get ('PATH_INFO', ''),
+		'tid': _TID,
+		'cid': request.headers.environ.get ('REMOTE_ADDR', ''),
+		'uip': request.headers.environ.get ('REMOTE_ADDR', ''),
+		'cn': request.args.get ('cn', ''),
+		'cs': request.args.get ('cs', ''),
+		'cm': request.args.get ('cm', ''),
+	}
+
+	p = requests.post ('https://www.google-analytics.com/collect', data = d)
+
+
+@app.route ('/')
+def index ():
+	abort (501)
+
+@app.route ('/<path:p>')
+def img (p):
+	return send_from_directory ('img', p)
+
+
